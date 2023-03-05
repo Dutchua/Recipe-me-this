a3ac328ac205c3a49919441057174116b3f54216
Josh <josh.jennings5@gmail.com>
2023-03-05 09:57:56 +0200

    Added Connect to DatabaseConnection.cs

--- recipe-me-this/recipe-me-this/Utility/DatabaseConnection.cs
+++ recipe-me-this/recipe-me-this/Utility/DatabaseConnection.cs
@@ -1,5 +1,19 @@
-﻿namespace recipe_me_this.Utility;
+﻿using Npgsql;
+namespace recipe_me_this.Utility;
 
 public class DatabaseConnection
 {
+    public static NpgsqlConnection Connection;
+    public static void Connect()
+    {
+        try
+        {
+            Connection = new NpgsqlConnection(Settings.connString);
+            Connection.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 }
